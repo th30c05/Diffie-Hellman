c8ab68c6c127a7afa994b306e57242c54439838f
th30c05 <arnaufugarolas@gmail.com>
2021-07-15 17:01:47 +0200

    Client improved:   - Before sending data generate a shared key   - Before sending the data, use the shared key to encrypt it.   - Sends encrypted data and everything needed to decrypt it except the shared key
    Signed-off-by: th30c05 <arnaufugarolas@gmail.com>

--- client.py
+++ client.py
@@ -1,78 +1,66 @@
-from base64 import b64encode, b64decode
-from json import dumps, loads
-from secrets import SystemRandom
-from socket import socket, AF_INET, SOCK_STREAM
-
-
-def packaging(Header, Data):
-    pack1 = ['header', 'data']
-    pack2 = (Header, Data)
-
-    pack = dumps(dict(zip(pack1, pack2)))
-
-    pack_b64 = b64encode(pack.encode("utf-8"))
-    return pack_b64
-
-
-def send_TCP(data_to_send, host, port):
-    global received
+def create_TCP(host, port):
+    from socket import socket, AF_INET, SOCK_STREAM
     tcp_client = socket(AF_INET, SOCK_STREAM)
 
     try:
-        # Establish connection to TCP server and exchange data
         tcp_client.connect((host, port))
-        tcp_client.sendall(data_to_send)
 
-        received = str(b64decode(tcp_client.recv(1024, ).decode("utf-8"))).removeprefix("b'").removesuffix("'")
+    except:
+        print("Error can't connect to " + host + ":" + port)
 
-    finally:
-        tcp_client.close()
-        return received
+    else:
+        return tcp_client
+
+
+def send_TCP(data_to_send, tcp_client):
+    tcp_client.send(data_to_send)
+
+    received = tcp_client.recv(1024, ).decode("utf-8")
+    return received
 
 
 def AES_key_gen(host, port):
     while True:
-        random = SystemRandom()
 
-        # Set Base and the Prime
-        Base = 2
-        Prime = int(
-            'F32D6D650FB74CDD13F737E5B8C48757325630FA755FAA91D1539',
-            16)
+        from functions import get_secret
+        Base, Prime, Secret, Pub = get_secret()
 
-        # Generate Secret
-        Secret = random.randint(int(
-            '26E4D30ECCC3215DD8F3157D27E23ACBDCFE68000000000000000',
-            16), int(
-            '184F03E93FF9F4DAA797ED6E38ED64BF6A1F00FFFFFFFFFFFFFFFF',
-            16))
+        tcp_client = create_TCP(host, port)
 
-        # Send the request with the pub key
-        package = send_TCP((packaging('NewKey', pow(Base, Secret, Prime))), host, port)
-
-        # Load json
-        package = loads(package)
+        from functions import packaging, depackaging
+        package = depackaging(send_TCP((packaging('NewKey', Pub)), tcp_client))
 
         # Generate the shared key
         shared_key = bytes(str(pow(package['data'], Secret, Prime)), 'UTF-8')
         if len(shared_key) == 64:
             break
 
-    return shared_key
+    return shared_key, tcp_client
 
 
 def main():
+    debug = input("Debug? (Y or N) ").upper()
+    if debug == "Y":
+        debug = True
+    else:
+        debug = False
+
     host_ip, server_port = str(input("Host: ")), int(input("Port: "))
-    # host_ip, server_port = "192.168.1.15", 9999
+
     while True:
+
         data = input("MSG: ")
-        if data == "NewKey":
-            shared_key = AES_key_gen(host_ip, server_port)
-            print(shared_key)
-        else:
-            received = send_TCP(packaging("Data", data), host_ip, server_port)
-            print("Bytes Sent: {}".format(data))
-            print("Response: {}".format(received))
+
+        shared_key, tcp_client = AES_key_gen(host_ip, server_port)
+
+        if debug: print(shared_key)
+
+        from functions import packaging, encrypt
+        from json import loads
+        package = packaging("Data", loads(encrypt(shared_key, data.encode('UTF-8'))))
+        tcp_client.send(package)
+        print("Bytes Sent: {}".format(data))
+        tcp_client.close()
 
 
 if __name__ == '__main__':
